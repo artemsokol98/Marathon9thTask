4d4491eb24a28551eaa2a5cbe5e800b8d5e6bc77
Artem Sokolovskiy <artemsokol98@gmail.com>
2023-07-25 15:42:09 +0300

    Initial Commit

---
+++ Marathon9thTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Marathon9thTask
+//
+//  Created by Артем Соколовский on 25.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
